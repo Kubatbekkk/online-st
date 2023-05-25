4896819a6e5dc6a4dc8bb727f0115cd63374d325
Kubatbekkk <kubineccastro@gmail.com>
2023-05-25 19:14:00 +0600

    fix: make footer sticky

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import PageContent from './components/PageContent';
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Navbar />
       <PageContent />
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import './styles.scss';
+import logo from '../../assets/logo.svg';
+import phoneIcon from '../../assets/icons/phone.svg';
+import addressIcon from '../../assets/icons/address.svg';
+import { links } from '../../data/nav-links';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="footer__container">
+        <div className="footer__logo">
+          <img src={logo} alt="logo" />
+        </div>
+
+        <ul className="footer__links">
+          {links.map((link) => (
+            <li className="footer__links-item" key={link.name}>
+              <a href={link.to}>{link.name}</a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="footer__phone">
+          <img src={phoneIcon} alt="phone" />
+          <p className="footer__phone-num">+7 (495) 555-55-55</p>
+        </div>
+        <div className="footer__address">
+          <img src={addressIcon} alt="address" />
+          <p className="footer__address-text">г. Москва, ул. Расплетина, 24</p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
