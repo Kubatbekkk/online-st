6961c57404b738b0c79658cd22b2b850d05befea
Kubatbekkk <kubineccastro@gmail.com>
2023-05-26 11:13:35 +0600

    feat: add Loader

--- src/components/CardList/Card/index.jsx
+++ src/components/CardList/Card/index.jsx
@@ -1,15 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles.scss';
 import { numFormat } from '../../../utils/numFormat';
+import Loader from '../../../components/Loader';
 
 const Card = ({ id, img, title, author, oldPrice, newPrice, isAvialable }) => {
-  const [cart, setCart] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [cart, setCart] = useState(
+    () => JSON.parse(localStorage.getItem('cart')) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }, [cart]);
 
   const handleCartItem = async (id) => {
+    setLoading(true);
     const response = await fetch(
       'https://jsonplaceholder.typicode.com/posts/1'
     );
     response.status === 200 ? setCart((prev) => [...prev, id]) : undefined;
+    setLoading(false);
   };
 
   const isItemInCart = cart.some((s) => s === id);
@@ -29,12 +39,19 @@ const Card = ({ id, img, title, author, oldPrice, newPrice, isAvialable }) => {
               )}
               <p className="card__price--new">{numFormat.format(newPrice)}</p>
             </div>
-            <button
-              className="btn btn--buy"
-              disabled={isItemInCart}
-              onClick={() => handleCartItem(id)}
-            >
-              {isItemInCart ? 'В корзине' : 'Купить'}
+            <button className="btn btn--buy" onClick={() => handleCartItem(id)}>
+              {/* {loading ? (
+                <img
+                  src={loaderIcon}
+                  alt="loading"
+                  className="card__loader-img"
+                />
+              ) : isItemInCart ? (
+                'В корзине'
+              ) : (
+                'Купить'
+              )} */}
+              <Loader className="loader-animate" />
             </button>
           </>
         ) : (
